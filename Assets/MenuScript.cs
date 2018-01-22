0acdc6dcea5ef1af5f63c669619430c322ef4d10
mw_prezes <mw.marcin.waligorski@gmail.com>
2018-01-22 23:30:25 +0100

    Menu
    zmienione menu

--- Assets/MenuScript.cs
+++ Assets/MenuScript.cs
@@ -6,31 +6,43 @@ using UnityEngine.SceneManagement;
 public class MenuScript : MonoBehaviour {
 
     public int window;
+	public GUISkin skin;
+	public float buttonWidth = 540;
+	public float buttonHeight = 90;
+	public float screenMargin = 50;
+	public Texture control;
+
+	private float buttonMargin = 10;
 
 	void Start ()
     {
         window = 1;
+		buttonWidth = (buttonWidth * Screen.width) / 1920;
+		buttonHeight = (buttonHeight * Screen.height) / 1080;
+		buttonMargin = (buttonMargin * Screen.height) / 1080;
+		screenMargin = (screenMargin * Screen.height) / 1080;
 	}
 
 
     void OnGUI()
     {
-        GUI.BeginGroup(new Rect(Screen.width / 2 - 100, Screen.height / 2 - 100, 200, 200));
+		GUI.skin = skin;
+		GUI.BeginGroup(new Rect(100, screenMargin, buttonWidth+50, (buttonHeight * buttonMargin)*4));
         if (window == 1)
         {
-            if (GUI.Button(new Rect(10, 30, 180, 30), "Play"))
+			if (GUI.Button(new Rect(0, 0, buttonWidth, buttonHeight), "PLAY"))
             {
                 window = 2;
             }
-            if (GUI.Button(new Rect(10, 70, 180, 30), "Settings"))
+			if (GUI.Button(new Rect(0, buttonHeight + buttonMargin, buttonWidth, buttonHeight), "CONTROL"))
             {
                 window = 3;
             }
-            if (GUI.Button(new Rect(10, 110, 180, 30), "Credits"))
+			if (GUI.Button(new Rect(0, (buttonHeight + buttonMargin)*2, buttonWidth, buttonHeight), "CREDITS"))
             {
                 window = 4;
             }
-            if (GUI.Button(new Rect(10, 150, 180, 30), "Exit"))
+			if (GUI.Button(new Rect(0, (buttonHeight + buttonMargin)*3, buttonWidth, buttonHeight), "EXIT"))
             {
                 window = 5;
             }
@@ -38,17 +50,22 @@ public class MenuScript : MonoBehaviour {
 
         if (window == 2)
         {
-            GUI.Label(new Rect(60, 10, 180, 30), "Choose level");
-            if (GUI.Button(new Rect(10, 40, 180, 30), "Test level"))
+			GUI.Label(new Rect(10, 10, buttonWidth+50, (buttonHeight * buttonMargin)*2), "CHOOSE LEVEL");
+			if (GUI.Button(new Rect(10, screenMargin, buttonWidth, buttonHeight), "MULTIPLAYER"))
             {
-                Debug.Log("Test level loaded");
-                SceneManager.LoadScene(1);
+                Debug.Log("Multi level loaded");
+                SceneManager.LoadScene("MultiplayerScene");
             }
+			if (GUI.Button(new Rect(10, buttonHeight + buttonMargin + screenMargin, buttonWidth, buttonHeight), "BACK TO MENU"))
+			{
+				window = 1;
+			}
         }
 
         if (window == 3)
         {
-            if (GUI.Button(new Rect(10, 160, 180, 30), "Back to menu"))
+			GUI.Label(new Rect (10, screenMargin, Screen.width/2, Screen.height/2), control);
+			if (GUI.Button(new Rect(10, buttonHeight * 7 + screenMargin, buttonWidth,  buttonHeight), "BACK TO MENU"))
             {
                 window = 1;
             }
@@ -56,14 +73,14 @@ public class MenuScript : MonoBehaviour {
 
         if (window == 4)
         {
-            GUI.Label(new Rect(10, 40, 180, 200), 
+			GUI.Label(new Rect(10, screenMargin, buttonWidth, buttonHeight*6), 
                 "Created by: \n" +
                 "Baida Bogdan \n" +
-                "Lukasz Suliborski \n" +
-                "Marcin Waligorski \n" +
-                "Piotr Olszewski \n" +
-                "Szejku Mateusz");
-            if (GUI.Button(new Rect(10, 150, 180, 30), "Back to menu"))
+				"Olszewski Piotr \n" +
+				"Suliborski Łukasz \n" +
+				"Szejner Mateusz \n" +
+				"Waligórski Marcin");
+			if (GUI.Button(new Rect(10, buttonHeight * 6 + screenMargin, buttonWidth, buttonHeight), "BACK TO MENU"))
             {
                 window = 1;
             }
